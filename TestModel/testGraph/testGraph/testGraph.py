edf81450f1029e6482cfb2633deed40c76f08278
Sasha <sasha.stepenov@plantandfood.co.nz>
2023-10-05 10:08:13 +1300

    correct path to observed

--- TestModel/testGraph/testGraph/testGraph.py
+++ TestModel/testGraph/testGraph/testGraph.py
@@ -12,10 +12,10 @@ path = os.getcwd()
 save_path = path
 
 #need this line to run on machine
-observed_path = os.path.join(path, "../../../../TestModel/Observed/observed.csv")
+#observed_path = os.path.join(path, "../../../../TestModel/Observed/observed.csv")
 
 #run this code for an action
-#observed_path = "TestModel/Observed/observed.csv"
+observed_path = "TestModel/Observed/observed.csv"
 
 observed_data = pd.read_csv(observed_path,index_col=0)
 
