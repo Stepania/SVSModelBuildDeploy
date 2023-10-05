84aeb5cad7a3af874f834b1a6e51aafaa2905878
Sasha <sasha.stepenov@plantandfood.co.nz>
2023-10-05 13:04:39 +1300

    no "'Date',inplace=True"

--- TestModel/testGraph/testGraph/testGraph.py
+++ TestModel/testGraph/testGraph/testGraph.py
@@ -39,7 +39,7 @@ for t in tests[:]:
  
     testframe = pd.read_csv(path + "\\OutputFiles\\"+t+".csv",index_col=0,delim_whitespace=True,dayfirst=True,date_format='%d/%m/%Y %H:%M:%S %p')    
     
-    testframe.set_index('Date',inplace=True)
+    #testframe.set_index('Date',inplace=True)
     Alltests.append(testframe)   
 
 
