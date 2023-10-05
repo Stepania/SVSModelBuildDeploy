22dbc43d4ddb2874c565278f26a0a08b64dcca6b
Sasha <sasha.stepenov@plantandfood.co.nz>
2023-10-05 13:34:48 +1300

    rerun for new errors

--- TestModel/testGraph/testGraph/testGraph.py
+++ TestModel/testGraph/testGraph/testGraph.py
@@ -32,54 +32,20 @@ new_file.write_text('Hello file')
 
 Alltests =[]
 for t in tests[:]:
-    
-    #Alltests.append(pd.read_csv( path + "\\OutputFiles\\"+t+".csv",index_col=0,dayfirst=True))    
-    #
-    #dateparse = lambda x: dt.datetime.strptime(x, '%d/%m/%Y %H:%M:%S %p')  
- 
-    testframe = pd.read_csv(path + "\\OutputFiles\\"+t+".csv",index_col=0,delim_whitespace=True,dayfirst=True,date_format='%d/%m/%Y %H:%M:%S %p')    
-    
-    #testframe.set_index('Date',inplace=True)
+     
+    testframe = pd.read_csv(path + "\\OutputFiles\\"+t+".csv",index_col=0,date_format='%d/%m/%Y %H:%M:%S %p')       
     Alltests.append(testframe)   
 
-
 AllData = pd.concat(Alltests,axis=1,keys=tests)
-
-#problem is here!
-#AllData.index = pd.to_datetime(AllData.index,format = "%d/%m/%Y %H:%M:%S %p")
-
-#debugging
-afterAllData = pathlib.Path(path, "afterAllData")
-afterAllData.mkdir(parents=True, exist_ok=True)
-new_file = afterAllData / 'myfile.txt'
-new_file.write_text('Hello file')
 
 
 observed_data.index=pd.to_datetime(observed_data.index,format="%d/%m/%Y %H:%M")
 
-#debugging
-afterobsrved = pathlib.Path(path, "afterobsrved")
-afterobsrved.mkdir(parents=True, exist_ok=True)
-new_file = afterobsrved / 'myfile.txt'
-new_file.write_text('Hello file')
-
-
 observed_test = observed_data.columns.get_level_values(0).drop_duplicates()
 AllData.sort_index(axis=0,inplace=True)
 
-#debugging
-beforeData = pathlib.Path(path, "beforeData")
-beforeData.mkdir(parents=True, exist_ok=True)
-new_file = beforeData / 'myfile.txt'
-new_file.write_text('Hello file')
-
 
 AllData.index = pd.to_datetime(AllData.index)
-#debugging
-afterData = pathlib.Path(path, "afterData")
-afterData.mkdir(parents=True, exist_ok=True)
-new_file = afterData / 'myfile.txt'
-new_file.write_text('Hello file')
 
 tests = AllData.columns.get_level_values(0).drop_duplicates()
 colors = pd.Series(['r','b','g'])
@@ -88,6 +54,7 @@ start = dt.datetime.date(AllData['test 1'].dropna().index.min())
 end = dt.datetime.date(AllData['test 1'].dropna().index.max())
 
 #debugging
+#we are here now
 insideLoop = pathlib.Path(path, "insideLoop")
 insideLoop.mkdir(parents=True, exist_ok=True)
 new_file = insideLoop / 'myfile.txt'
