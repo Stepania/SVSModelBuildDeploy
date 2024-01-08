1de767c9913c8747567cef44278910dace500597
Hamish Brown <Hamish.Brown@plantandfood.co.nz>
2024-01-08 18:31:01 +1300

    Tidy up test graphs so can edit with Jupyter

--- TestGraphs/ResidueSensibilityGraphs.py
+++ TestGraphs/ResidueSensibilityGraphs.py
@@ -1,28 +1,65 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+import os
 import datetime as dt
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import MathsUtilities as MUte
 import matplotlib.dates as mdates
 
-ResidueTestNames = ['StovN0.5High.csv','StovN1.0High.csv','StovN1.5High.csv','StovN3.0High.csv','StovN4.5High.csv',
-                    'StovN0.5Low.csv','StovN1.0Low.csv','StovN1.5Low.csv','StovN3.0Low.csv','StovN4.5Low.csv']
-cols = ['k','b','g','y','r','k','b','g','y','r']
-lines = ['-','-','-','-','-','--','--','--','--','--']
+# Path for current Tests
 
-ResidueTests = []
-for t in ResidueTestNames:
-    rt = pd.read_csv('C:\\GitHubRepos\\SVSModelBuildDeploy\\TestModel\\TestConsole\\bin\\Debug\\net6.0\\OutputFiles\\'+t,index_col="Date",date_format='%d/%m/%Y %H:%M:%S %p')
-    ResidueTests.append(rt)
-AllResidueTests = pd.concat(ResidueTests,axis=0,keys=ResidueTestNames)
+path = os.getcwd()[:-11] +"\\TestComponents\\TestSets\\Residues"
+
+# Get names and results from each test
+
+tests = []
+test_name = []
+for file in os.listdir(path+"\\Outputs"):
+    if file.endswith('.csv'):
+        tests.append(file)       
+        test_name.append(os.path.splitext(file)[0])
+
+# Pack tests up into dataframe for graphing
+
+# +
+Alltests =[]
+for t in tests[:]:  
+    testframe = pd.read_csv(path+"\\Outputs\\"+t,index_col=0,dayfirst=True,date_format='%d/%m/%Y %H:%M:%S %p')  
+    Alltests.append(testframe)   
+
+AllData = pd.concat(Alltests,axis=1,keys=test_name)
+AllData.sort_index(axis=0,inplace=True)
+AllData.index = pd.to_datetime(AllData.index)
+# -
+
+# Make graph
+
+# +
+cols = ['k','k','b','b','g','g','y','y','r','r']
+lines = ['-','--','-','--','-','--','-','--','-','--',]
 
 Graph = plt.figure()
 ax = Graph.add_subplot(1,1,1)
 pos = 0
-for t in ResidueTestNames:
-    plt.plot(AllResidueTests.loc[t,'ResidueN'].cumsum(),lines[pos],color=cols[pos],label = t[:-4])
+for t in test_name:
+    plt.plot(AllData.loc[:,(t,'ResidueN')].cumsum(),lines[pos],color=cols[pos],label = t)
     pos +=1
 plt.legend(loc=(1.1,0.2))
 plt.ylabel('Cum Net Residue mineralisation (kg/ha)')
-
-plt.show()
+plt.xticks(rotation=60)
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%#d-%b'))
+plt.savefig(path+'\\TimeCourse.png')
--- TestGraphs/WS2Tests.py
+++ TestGraphs/WS2Tests.py
@@ -1,58 +1,55 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+#from io import BytesIO
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
-#import aspose.words as aw
 
-path = os.getcwd()
+path = os.getcwd()[:-11] +"\\TestComponents\\TestSets\\WS2"
 
-#run this on machine
-observed_path = os.path.join(path, "../../../../TestComponents/Observed/observed.csv")
-
-observed_data = pd.read_csv(observed_path,index_col=0)
-
+observed_data = pd.read_csv(path + "\\observed.csv",index_col=0)
 observed_data.sort_index(axis=0,inplace=True)
-
-tests = []
-test_name = []
-out_path = path+"\\Outputfiles"
-for file in os.listdir(path+"\\Outputfiles"):
-    if file.endswith('.csv'):
-        tests.append(file)       
-        test_name.append(os.path.splitext(file)[0])
-
-Alltests =[]
-for t in tests[:]:  
-    testframe = pd.read_csv(out_path+"\\"+t,index_col=0,dayfirst=True,date_format='%d/%m/%Y %H:%M:%S %p')  
-    
-    Alltests.append(testframe)   
-
-AllData = pd.concat(Alltests,axis=1,keys=test_name)
-
 observed_data.index=pd.to_datetime(observed_data.index,format="%d/%m/%Y %H:%M")
 
-observed_test = observed_data.columns.get_level_values(0).drop_duplicates()
-AllData.sort_index(axis=0,inplace=True)
+tests = []
+test_names = []
+for file in os.listdir(path+"\\Outputs"):
+    if file.endswith('.csv'):
+        tests.append(file)       
+        test_names.append(os.path.splitext(file)[0])
 
+# +
+Alltests =[]
+for t in tests[:]:  
+    testframe = pd.read_csv(path+"\\Outputs\\"+t,index_col=0,dayfirst=True,date_format='%d/%m/%Y %H:%M:%S %p')  
+    Alltests.append(testframe)   
+
+AllData = pd.concat(Alltests,axis=1,keys=test_names)
+AllData.sort_index(axis=0,inplace=True)
 AllData.index = pd.to_datetime(AllData.index)
 
-tests = AllData.columns.get_level_values(0).drop_duplicates()
+# +
 colors = pd.Series(['r','b','g'])
-
-start = dt.datetime.date(AllData['8Wheat'].dropna().index.min())
-end = dt.datetime.date(AllData['8Wheat'].dropna().index.max())
-
-def makeplot(Data,color):
-    plt.plot(Data,color=color)
-    
-def make_observed(observed):
-    plt.plot(observed.index,observed.loc[:,'Nitrogen'],'*',color='g')     
         
 Graph = plt.figure(figsize=(10,10))
 pos = 1
-row_num=len(tests)
+row_num=len(test_names)
 
-for t in tests:
+for t in test_names:
     start = dt.datetime.date(AllData[t].dropna().index.min())
     end = dt.datetime.date(AllData[t].dropna().index.max())
     datefilter = []
@@ -68,8 +65,8 @@ for t in tests:
     Data = AllData.loc[:,(t,'SoilMineralN')].sort_index()
     plt.xticks(rotation = 45)    
     plt.title("SoilMineralN")
-    makeplot(Data,color)
-    make_observed(observed_data[datefilter])
+    plt.plot(Data,color=color)
+    #make_observed(observed_data[datefilter])
     Graph.tight_layout(pad=1.5)
     pos+=1
     
@@ -77,15 +74,12 @@ for t in tests:
     plt.xticks(rotation = 45)  
     plt.title("CropN")
     Data = AllData.loc[:,(t,'CropN')].sort_index()
-    makeplot(Data,color)
-    make_observed(observed_data[datefilter])
+    plt.plot(Data,color=color)
+    #make_observed(observed_data[datefilter])
     pos+=1
 
-plt.savefig('testplot.png')
+plt.savefig(path+'\\TimeCourse.png')
 
-#doc = aw.Document()
-#builder = aw.DocumentBuilder(doc)
-#builder.insert_image("testplot.png")
-#doc.save("index.html")
+# -
 
-plt.show() 
+
